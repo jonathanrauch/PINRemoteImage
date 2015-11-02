a17c5e3952d247d8363fb365aa531f5620dd0e08
Jonathan Rauch <jonathan@Jonathans-MacBook-Pro-2.local>
2015-11-02 12:11:03 +0000

    Added MKAnnotationView PINRemoteImageCategory

---
+++ Pod/Classes/Image Categories/MKAnnotationView+PINRemoteImage.h
@@ -0,0 +1,13 @@
+//
+//  MKAnnotationView+PINRemoteImage.h
+//  Pods
+//
+//  Created by Jonathan Rauch on 02/11/2015.
+//
+
+#import "PINRemoteImageCategoryManager.h"
+@import MapKit;
+
+@interface MKAnnotationView (PINRemoteImage) <PINRemoteImageCategory>
+
+@end
